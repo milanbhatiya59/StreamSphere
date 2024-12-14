1142d8bbaff3292f08504bd93fb02c8d92757f5c
milanbhatiya59 <milanbhatiya59@gmail.com>
2024-12-15 02:18:01 +0530

    database connected

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const dbConnect = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+console.log("\n\n" + prismaClientSingleton() + "\n\n");
+
+export default dbConnect;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = dbConnect;
