6a62bef920a48874b4e0ba5373e16599377930d7
milanbhatiya59 <milanbhatiya59@gmail.com>
2025-01-02 13:03:18 +0530

    deployment error resolved 05

--- src/app/(browse)/[username]/page.tsx
+++ src/app/(browse)/[username]/page.tsx
@@ -4,16 +4,13 @@ import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
 
-interface UserPageProps {
-    params: {
-        username: string;
-    };
-}
+type Params = Promise<{ username: string }>;
 
 export default async function UserPage({
     params,
-}: UserPageProps) {
-
+}: {
+    params: Params;
+}) {
     const { username } = await params;
 
     const user = await getUserByUsername(username);
